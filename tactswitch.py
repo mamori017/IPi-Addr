0dd5fac9490f494523f7fbac813e5a3c07f2b965
mamori017 <masahikomori@outlook.jp>
2017-03-04 22:00:26 +0900

    Test commit

---
+++ tactswitch.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import shlex
+import subprocess
+import time
+
+# How to wire
+# https://cloud.githubusercontent.com/assets/7507701/23578924/e4e06080-0124-11e7-932e-c00e3bdaf5b1.png
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.IN)
+
+def execute():
+  objProc = subprocess.Popen(shlex.split("date"))
+
+try:
+  GPIO.wait_for_edge(25, GPIO.FALLING)
+except:
+  print "Error!"
+finally:
+  GPIO.cleanup()
+
+execute()
